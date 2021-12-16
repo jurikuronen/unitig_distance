7b4de0d0c9fc61ef21c2c6227866724e1c5f7b5e
Juri Kuronen <juri.kuronen@gmail.com>
2021-12-16 19:13:15 +0200

    Clean up.

--- include/unitig_distance/Timer.hpp
+++ include/unitig_distance/Timer.hpp
@@ -13,17 +13,29 @@
 class Timer {
 public:
     using clock = std::chrono::high_resolution_clock;
+
     Timer() : m_start(now()), m_mark(m_start), m_stopwatch(clock::duration()) { }
+
     void set_mark() { m_mark = now(); }
+
     void add_time_since_mark() { m_stopwatch += time_elapsed(m_mark); }
+
     std::string get_stopwatch_time() const { return get_time_str(m_stopwatch); }
+
     std::string get_time_since_start() const { return get_time_str(time_elapsed(m_start)); }
+
     std::string get_time_since_mark() const { return get_time_str(time_elapsed(m_mark)); }
+
     std::string get_time_since_start_and_set_mark() { set_mark(); return get_time_since_start(); }
+
     std::string get_time_since_mark_and_set_mark() { auto time_str = get_time_since_mark(); set_mark(); return time_str; }
+
     std::string get_time_block_since_start() const { return get_time_block(time_elapsed(m_start)); }
+
     std::string get_time_block_since_mark() const { return get_time_block(time_elapsed(m_mark)); }
+
     std::string get_time_block_since_start_and_set_mark() { set_mark(); return get_time_block_since_start(); }
+
     std::string get_time_block_since_mark_and_set_mark() { auto time_block = get_time_block_since_mark(); set_mark(); return time_block; }
 
 private:
@@ -32,28 +44,50 @@ private:
     clock::duration m_stopwatch; // Accumulates time.
 
     clock::time_point now() const { return clock::now(); }
+
     clock::duration time_elapsed(const clock::time_point& tp) const { return now() - tp; }
+
     std::string get_time_block(const clock::duration& t) const {
         std::ostringstream oss;
+
         int_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(t).count();
-        auto s = ms / 1000 % 60;
-        auto m = ms / 1000 / 60 % 60;
-        auto h = ms / 1000 / 60 / 60 % 24;
-        auto d = ms / 1000 / 60 / 60 / 24;
+        int_t s = ms / 1000 % 60;
+        int_t m = ms / 1000 / 60 % 60;
+        int_t h = ms / 1000 / 60 / 60 % 24;
+        int_t d = ms / 1000 / 60 / 60 / 24;
         ms %= 1000;
+
         oss << "[";
         if (d) oss << pad(d) << "d " << pad(h) << "h";
         else if (h) oss << pad(h) << "h " << pad(m) << "m";
         else if (m) oss << pad(m) << "m " << pad(s) << "s";
         else oss << pad(s) << "." << zeropad(ms) << "s";
         oss << "]";
+
         return oss.str();
     }
+
     std::string get_time_str(const clock::duration& t) const {
-        auto str = get_time_block(t);
-        return std::string(str.begin() + 1 + (str[1] == ' '), str.end() - 1);
+        std::ostringstream oss;
+
+        int_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(t).count();
+        int_t s = ms / 1000 % 60;
+        int_t m = ms / 1000 / 60 % 60;
+        int_t h = ms / 1000 / 60 / 60 % 24;
+        int_t d = ms / 1000 / 60 / 60 / 24;
+        ms %= 1000;
+
+        if (d) oss << d << "d " << h << "h";
+        else if (h) oss << h << "h " << m << "m";
+        else if (m) oss << m << "m " << s << "s";
+        else oss << s << "." << zeropad(ms) << "s";
+
+        return oss.str();
     }
+
     std::string pad(int_t t) const { return t < 10 ? " " + std::to_string(t) : std::to_string(t); }
+
     std::string zeropad(int_t t) const { return t < 10 ? "00" + std::to_string(t) : (t < 100 ? "0" + std::to_string(t) : std::to_string(t)); }
+
 };
 
