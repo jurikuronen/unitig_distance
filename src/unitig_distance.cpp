25338597e988a75e0d9686b04c5bcd99a34c1fb6
Juri Kuronen <juri.kuronen@gmail.com>
2021-12-22 15:59:59 +0200

    Old change got reverted accidentally.

--- src/unitig_distance.cpp
+++ src/unitig_distance.cpp
@@ -94,7 +94,7 @@ static void determine_outliers(const Queries& queries,
 {
     timer.set_mark();
     OutlierTools ot(queries, distances, counts, po.sgg_count_threshold(), po.max_distance(), po.output_one_based(), po.verbose());
-    if (po.outlier_threshold() >= 0.0) {
+    if (ld_distance() >= 0 && outlier_threshold() >= 0.0) {
         ot.determine_outliers(po.ld_distance(), po.outlier_threshold());
     } else {
         ot.determine_outliers(po.ld_distance(), po.ld_distance_nth_score(), po.ld_distance_min(), po.ld_distance_score());
