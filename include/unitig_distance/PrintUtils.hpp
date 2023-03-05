324b5a71eef44d87776fc091f94475b14d077ecf
Juri Kuronen <juri.kuronen@gmail.com>
2023-03-05 20:48:49 +0200

    Add comments.

--- include/unitig_distance/PrintUtils.hpp
+++ include/unitig_distance/PrintUtils.hpp
@@ -8,6 +8,7 @@ class PrintUtils {
 public:
     static void print_license() { std::cout << "unitig_distance | MIT License | Copyright (c) 2020-2022 Juri Kuronen\n\n"; }
 
+    // Time block since start.
     template<typename... Ts>
     static void print_tbss(const Timer& timer, Ts... ts) {
         tbss(timer);
@@ -15,6 +16,7 @@ public:
         dot_endl();
     }
 
+    // Time block since start, and set mark.
     template<typename... Ts>
     static void print_tbssasm(Timer& timer, Ts... ts) {
         tbssasm(timer);
@@ -22,14 +24,7 @@ public:
         dot_endl();
     }
 
-    template <typename... Ts>
-    static void print_tbss_tsmasm_noendl(Timer& timer, Ts... ts) {
-        tbss(timer);
-        print(ts...);
-        tsmasm(timer);
-        dot_noendl();
-    }
-
+    // Time block since start, time since mark.
     template <typename... Ts>
     static void print_tbss_tsm(const Timer& timer, Ts... ts) {
         tbss(timer);
@@ -38,12 +33,22 @@ public:
         dot_endl();
     }
 
+    // Time block since start, time since mark, and set mark.
     template <typename... Ts>
     static void print_tbss_tsmasm(Timer& timer, Ts... ts) {
         tbss(timer);
         print(ts...);
         tsmasm(timer);
         dot_endl();
+    }
+
+    // Time block since start, time since mark, and set mark, no line break.
+    template <typename... Ts>
+    static void print_tbss_tsmasm_noendl(Timer& timer, Ts... ts) {
+        tbss(timer);
+        print(ts...);
+        tsmasm(timer);
+        dot_noendl();
     }
 
 private:
@@ -53,9 +58,13 @@ private:
     template <typename T, typename... Ts>
     static void print(T t, Ts... ts) { print(t); print(ts...); }
 
+    // Time block since start.
     static void tbss(const Timer& timer) { std::cout << timer.get_time_block_since_start(); }
+    // Time block since start, and set mark.
     static void tbssasm(Timer& timer) { std::cout << timer.get_time_block_since_start_and_set_mark(); }
+    // Time since mark.
     static void tsm(const Timer& timer) { print("in", timer.get_time_since_mark()); }
+    // Time since mark, and set mark.
     static void tsmasm(Timer& timer) { print("in", timer.get_time_since_mark_and_set_mark()); }
 
     static void dot_endl() { std::cout << '.' << std::endl; }
